d7c33f9ccd4d277fdb03ecad173ed83cdb321ab4
jaan-c <jaan-c@users.noreply.github.com>
2022-07-11 05:44:02 +0800

    Implement grouping logic.

---
+++ imgdedup/deduplicate.py
@@ -0,0 +1,48 @@
+from typing import *
+import statistics
+import dataclasses
+from imgdedup.phash import phash_image_file, hamming_distance_percent
+
+
+@dataclasses.dataclass(frozen=True)
+class _Image:
+    path: str
+    phash: int
+
+
+def group_duplicate_images(
+    paths: Iterable[str],
+    threshold: float,
+) -> List[List[str]]:
+    images: List[_Image] = []
+    for p in paths:
+        phash = phash_image_file(p)
+        images.append(_Image(p, phash))
+
+    groups: List[List[_Image]] = []
+    for image in images:
+        fit_group_ix = -1
+        for ix, group in enumerate(groups):
+            phash_group = map(lambda i: i.phash, group)
+            average_distance = _average_hamming_distance_percent(
+                phash,
+                phash_group,
+            )
+            if average_distance <= threshold:
+                fit_group_ix = ix
+                break
+
+        if fit_group_ix > -1:
+            groups[fit_group_ix].append(image)
+        else:
+            groups.append([image])
+
+    return [[i.path for i in g] for g in groups]
+
+
+def _average_hamming_distance_percent(
+    phash: int,
+    phash_group: Iterable[int],
+) -> float:
+    distances = map(lambda p: hamming_distance_percent(phash, p), phash_group)
+    return statistics.fmean(distances)
